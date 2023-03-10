76fa3ba45ecece0afe5f75dee613996c5696dab6
Kandasamy <kandazhmail@gmail.com>
2023-03-10 16:50:32 +0530

    new changes

--- models/example/my_first_dbt_model.sql
+++ models/example/my_first_dbt_model.sql
@@ -20,6 +20,16 @@ with source_data as (
     select 3 as id
         union all 
     select 4 as id
+union all
+        select 1 as id
+    union all
+    select null as id
+    union all 
+    select 2 as id
+    union all 
+    select 3 as id
+        union all 
+    select 4 as id
 
 )
 
